ddad321bf1724a927aabbc0cdd56caf3a9def85f
Yagnen27 <yagnen27barot@gmail.com>
2024-05-31 17:58:35 +0530

    Avo Route Is Working

---
+++ app/avo/resources/user.rb
@@ -0,0 +1,13 @@
+class Avo::Resources::User < Avo::BaseResource
+  self.title = :id
+  self.includes = []
+  # self.search = {
+  #   query: -> { query.ransack(id_eq: params[:q], m: "or").result(distinct: false) }
+  # }
+
+  def fields
+    field :id, as: :id
+    field :name, as: :text
+    field :email, as: :text
+  end
+end
